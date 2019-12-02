d983c95dce1074771beb507188acaf7dd718cce3
Kateka Seth <katekaseth@gmail.com>
2019-12-02 13:24:32 -0800

    deploy client on http

--- client/deploy_commands.sh
+++ client/deploy_commands.sh
@@ -6,7 +6,5 @@ docker run -d \
 -p 443:443 \
 --name client \
 -v /etc/letsencrypt/:/etc/letsencrypt/:ro \
--e TLSCERT=/etc/letsencrypt/live/snake.katekaseth.me/fullchain.pem \
--e TLSKEY=/etc/letsencrypt/live/snake.katekaseth.me/privkey.pem \
 --restart unless-stopped \
 kateks/client
