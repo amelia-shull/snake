62a48ec7a2a3ca8d728b88fc7a88dfc41e4e183f
Kateka Seth <katekaseth@gmail.com>
2019-12-02 14:12:00 -0800

    deploy on https

--- client/deploy_commands.sh
+++ client/deploy_commands.sh
@@ -3,7 +3,9 @@ docker rm -f client
 docker pull kateks/client
 docker run -d \
 -p 80:80 \
+-p 443:443 \
 --name client \
 -v /etc/letsencrypt/:/etc/letsencrypt/:ro \
---restart unless-stopped \
+-e TLSCERT=/etc/letsencrypt/live/retrosnake.me/fullchain.pem \
+-e TLSKEY=/etc/letsencrypt/live/retrosnake.me/privkey.pem \
 kateks/client
