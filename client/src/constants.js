4b3719c4068015670fb0f3faaee08b844d566dca
Kateka Seth <katekaseth@gmail.com>
2019-12-02 16:38:57 -0800

    change master to localhost

--- client/src/constants.js
+++ client/src/constants.js
@@ -4,7 +4,9 @@ module.exports = Object.freeze({
         MAIN: 'main'
     },
     URL : {
-        BASE_URL: 'https://snakeapi.katekaseth.me/',
-        BASE_URL_WS: 'wss://snakeapi.katekaseth.me/'
+        // BASE_URL: 'http://snakeapi.katekaseth.me/',
+        // BASE_URL_WS: 'wss://snakeapi.katekaseth.me/'
+        BASE_URL: 'http://localhost:8844/',
+        BASE_URL_WS: 'ws://localhost:8844/'
     }
 });
